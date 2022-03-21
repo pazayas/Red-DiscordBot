ebb5ec5b60feb4d16048ffafc2c3f798fa6aa4bf
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-03-21 12:19:27 +0100

    Modernize cogs by using async cog_load and cog_unload

--- redbot/cogs/audio/core/events/dpy.py
+++ redbot/cogs/audio/core/events/dpy.py
@@ -245,11 +245,11 @@ class DpyEvents(MixinMeta, metaclass=CompositeMetaClass):
         if not handled:
             await self.bot.on_command_error(ctx, error, unhandled_by_cog=True)
 
-    def cog_unload(self) -> None:
+    async def cog_unload(self) -> None:
         if not self.cog_cleaned_up:
             self.bot.dispatch("red_audio_unload", self)
-            self.session.detach()
-            asyncio.create_task(self._close_database()).add_done_callback(task_callback_trace)
+            await self.session.close()
+            await self._close_database()
             if self.player_automated_timer_task:
                 self.player_automated_timer_task.cancel()
 
@@ -264,11 +264,9 @@ class DpyEvents(MixinMeta, metaclass=CompositeMetaClass):
 
             lavalink.unregister_event_listener(self.lavalink_event_handler)
             lavalink.unregister_update_listener(self.lavalink_update_handler)
-            asyncio.create_task(lavalink.close(self.bot)).add_done_callback(task_callback_trace)
+            await lavalink.close(self.bot)
             if self.player_manager is not None:
-                asyncio.create_task(self.player_manager.shutdown()).add_done_callback(
-                    task_callback_trace
-                )
+                await self.player_manager.shutdown()
 
             self.cog_cleaned_up = True
 
